e59a28c671185f63714e73d8b570412445ae087b
Deklan Dieterly <deklan.dieterly@hpe.com>
2016-10-03 14:36:42 -0600

    Removed unused imports

--- src/main/java/MPerf.java
+++ src/main/java/MPerf.java
@@ -13,7 +13,6 @@ import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.PreparedStatement;
 import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.ResultSetFuture;
-import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Session;
 import com.datastax.driver.core.SocketOptions;
 
